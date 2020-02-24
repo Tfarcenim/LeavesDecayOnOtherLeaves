0574e62a82837bf1e19a6b851679a7ea5db109e4
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2020-02-24 14:21:43 -0500

    Initial commit

---
+++ src/main/java/tfar/leavesdecayonotherlogs/LeavesDecayOnOtherLeaves.java
@@ -0,0 +1,38 @@
+package tfar.leavesdecayonotherlogs;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
+import net.minecraftforge.fml.common.Mod;
+
+import static net.minecraft.block.LeavesBlock.DISTANCE;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(LeavesDecayOnOtherLeaves.MODID)
+public class LeavesDecayOnOtherLeaves {
+    public static final String MODID = "leavesdecayonotherleaves";
+    public static int getDistance(BlockState state, BlockState neighbor) {
+        if (BlockTags.LOGS.contains(neighbor.getBlock())) {
+            return 0;
+        } else {
+            return neighbor.getBlock() == state.getBlock() ? neighbor.get(DISTANCE) : 7;
+        }
+    }
+
+    public static BlockState handleDecay(BlockState state, IWorld worldIn, BlockPos pos) {
+        int i = 7;
+        try (
+                BlockPos.PooledMutable blockpos$pooledmutable = BlockPos.PooledMutable.retain()) {
+            for (Direction direction : Direction.values()) {
+                blockpos$pooledmutable.setPos(pos).move(direction);
+                i = Math.min(i, getDistance(state,worldIn.getBlockState(blockpos$pooledmutable)) + 1);
+                if (i == 1) {
+                    break;
+                }
+            }
+        }
+        return state.with(DISTANCE, i);
+    }
+}
---
+++ src/main/java/tfar/leavesdecayonotherlogs/mixin/MixinLeavesBlock.java
@@ -0,0 +1,23 @@
+package tfar.leavesdecayonotherlogs.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.LeavesBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import tfar.leavesdecayonotherlogs.LeavesDecayOnOtherLeaves;
+
+import static net.minecraft.block.LeavesBlock.DISTANCE;
+
+@Mixin(LeavesBlock.class)
+public abstract class MixinLeavesBlock {
+
+	@Inject(method = "updateDistance",at = @At("RETURN"),cancellable = true)
+	private static void exactMatch(BlockState state, IWorld worldIn, BlockPos pos, CallbackInfoReturnable<BlockState> cir){
+		if (cir.getReturnValue().get(DISTANCE) == 7)return;
+		cir.setReturnValue(LeavesDecayOnOtherLeaves.handleDecay(state,worldIn,pos));
+	}
+}
